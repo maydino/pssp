c4045457bcb11afb8e710c034662adfece6b0d30
maydino <mutluaydin.m@gmail.com>
2022-07-18 22:54:07 -0700

    First Commit

---
+++ PSSP/HomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HomeViewController.swift
+//  PSSP
+//
+//  Created by Mutlu Aydin on 7/18/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemRed
+        
+    }
+    
+
+   
+
+}
---
+++ PSSP/StatsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  StatsViewController.swift
+//  PSSP
+//
+//  Created by Mutlu Aydin on 7/18/22.
+//
+
+import UIKit
+
+class StatsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemGray
+    }
+ 
+
+}
