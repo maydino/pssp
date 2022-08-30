4dcbb506fcc613057abf9c027d2a7c1ba9c3d13f
maydino <mutluaydin.m@gmail.com>
2022-08-29 23:51:43 -0700

    Work on push up View Controller

--- PSSP/Controllers/PSSP/PushUpViewController.swift
+++ PSSP/Controllers/PSSP/PushUpViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class PushUpViewController: UIViewController {
 
+    var sevenDaysAverage = UtilityFuncs()
+    
     private let pushUpImage: UIImageView = {
         let image = UIImageView()
         image.image = UIImage(named: "pushUp")
@@ -16,16 +18,23 @@ class PushUpViewController: UIViewController {
         return image
     }()
     
+    private let enterCompletedLabel = PSSPLabel()
     private let pushUpTextField = PSSPTextField()
     private let sevenDaysAvgLabel = PSSPLabel()
+    private let thirtyDaysAvgLabel = PSSPLabel()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .psspBackgroundColor
+        
         pushUpImageConfiguration()
+        completedLabelConfiguration()
+
         pushUpTextFieldConfiguration()
         sevenDaysAvgLabelConfiguration()
+        thirtyDaysAvgLabelConfiguration()
 
     }
    
@@ -35,49 +44,69 @@ class PushUpViewController: UIViewController {
         pushUpImage.layer.masksToBounds = true
         
         NSLayoutConstraint.activate([
-            
             pushUpImage.widthAnchor.constraint(equalToConstant: view.bounds.width*0.6),
             pushUpImage.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
             pushUpImage.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
             pushUpImage.heightAnchor.constraint(equalToConstant: view.bounds.width*0.6)
+        ])
+    }
+    
+    func completedLabelConfiguration() {
+        enterCompletedLabel.translatesAutoresizingMaskIntoConstraints = false
+        enterCompletedLabel.text = "Enter Completed Push Ups"
+        enterCompletedLabel.textAlignment = .center
+                
+        view.addSubview(enterCompletedLabel)
         
+        NSLayoutConstraint.activate([
+            enterCompletedLabel.widthAnchor.constraint(equalToConstant: view.bounds.width*0.8),
+            enterCompletedLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
+            enterCompletedLabel.topAnchor.constraint(equalTo: pushUpImage.bottomAnchor, constant: 25),
+            enterCompletedLabel.heightAnchor.constraint(equalToConstant: 25)
         ])
     }
     
     func pushUpTextFieldConfiguration() {
         pushUpTextField.translatesAutoresizingMaskIntoConstraints = false
-        
-        pushUpTextField.dropShadow(color: .shadowColor!, opacity: 0.5, offSet: CGSize(width: 5, height: 5), radius: 6)
-        
+
         view.addSubview(pushUpTextField)
         
         NSLayoutConstraint.activate([
-            
-            pushUpTextField.widthAnchor.constraint(equalToConstant: 150),
-            pushUpTextField.topAnchor.constraint(equalTo: pushUpImage.bottomAnchor, constant: 25),
+            pushUpTextField.widthAnchor.constraint(equalToConstant: 100),
+            pushUpTextField.topAnchor.constraint(equalTo: enterCompletedLabel.bottomAnchor, constant: 10),
             pushUpTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
             pushUpTextField.heightAnchor.constraint(equalToConstant: 50)
-        
         ])
-        
     }
     
     func sevenDaysAvgLabelConfiguration() {
         sevenDaysAvgLabel.translatesAutoresizingMaskIntoConstraints = false
-        sevenDaysAvgLabel.text = "Seven Days Average: 100 "
-        
-        sevenDaysAvgLabel.dropShadow(color: .shadowColor!, opacity: 0.5, offSet: CGSize(width: 5, height: 5), radius: 6)
-        
+        sevenDaysAvgLabel.text = "Seven Days: \(sevenDaysAverage.sevenDaysAverage()) "
+                
         view.addSubview(sevenDaysAvgLabel)
         
         NSLayoutConstraint.activate([
-            
             sevenDaysAvgLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100),
             sevenDaysAvgLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -100),
-            sevenDaysAvgLabel.topAnchor.constraint(equalTo: pushUpTextField.bottomAnchor, constant: 25),
-            sevenDaysAvgLabel.heightAnchor.constraint(equalToConstant: 50)
-        
+            sevenDaysAvgLabel.topAnchor.constraint(equalTo: pushUpTextField.bottomAnchor, constant: 50),
+            sevenDaysAvgLabel.heightAnchor.constraint(equalToConstant: 25)
         ])
-        
     }
+    
+    func thirtyDaysAvgLabelConfiguration() {
+        thirtyDaysAvgLabel.translatesAutoresizingMaskIntoConstraints = false
+        thirtyDaysAvgLabel.text = "Thirty Days: \(sevenDaysAverage.sevenDaysAverage()) "
+        
+        view.addSubview(thirtyDaysAvgLabel)
+        
+        NSLayoutConstraint.activate([
+            thirtyDaysAvgLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100),
+            thirtyDaysAvgLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -100),
+            thirtyDaysAvgLabel.topAnchor.constraint(equalTo: sevenDaysAvgLabel.bottomAnchor, constant: 25),
+            thirtyDaysAvgLabel.heightAnchor.constraint(equalToConstant: 25)
+        ])
+    }
+    
+    
+
 }
---
+++ PSSP/Utilities/UtilityFuncs.swift
@@ -0,0 +1,20 @@
+//
+//  UtilityFuncs.swift
+//  PSSP
+//
+//  Created by Mutlu Aydin on 8/29/22.
+//
+
+import UIKit
+
+
+struct UtilityFuncs {
+    
+    
+    
+    mutating func sevenDaysAverage () -> Int {
+        
+        return 7
+    }
+    
+}
